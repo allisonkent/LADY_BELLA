a9da19a3c57b227b017e99b4c3a8630cc80076d7
SNOWBIRD XMD <praiseluke47@gmail.com>
2025-07-26 15:05:41 +0200

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: '𝗟𝗮𝗱𝘆_𝗕𝗲𝗹𝗹𝗮🎀',
+  author: '𝚂𝙽𝙾𝚆𝙱𝙸𝚁𝙳',
+  botName: "𝗟𝗮𝗱𝘆_𝗕𝗲𝗹𝗹𝗮🎀",
+  botOwner: 'SNOWBIRD', // Your name
+  ownerNumber: '263780145644', //Set your number here without + symbol.
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "1.0.0",
+};
+
+module.exports = settings;
