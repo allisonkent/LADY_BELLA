87a760243f1fc856c08b522f84176a6293d03c5a
allisonkent <jimohmm25012519@gmail.com>
2025-07-30 01:41:12 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '𝚂𝙽𝙾𝚆𝙱𝙸𝚁𝙳',
   botName: "𝗟𝗮𝗱𝘆_𝗕𝗲𝗹𝗹𝗮🎀",
   botOwner: 'SNOWBIRD', // Your name
-  ownerNumber: '263780145644', //Set your number here without + symbol.
+  ownerNumber: '2347015424938', //Set your number here without + symbol.
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
