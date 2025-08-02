f8634cd57cdaa9880192c5d15202d099594b4a3c
allisonkent <jimohmm25012519@gmail.com>
2025-08-02 17:59:46 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const settings = {
   packname: '𝗟𝗮𝗱𝘆_𝗕𝗲𝗹𝗹𝗮🎀',
   author: '𝚂𝙽𝙾𝚆𝙱𝙸𝚁𝙳',
   botName: "𝗟𝗮𝗱𝘆_𝗕𝗲𝗹𝗹𝗮🎀",
-  botOwner: 'SNOWBIRD', // Your name
+  botOwner: 'laykon_xmd', // Your name
   ownerNumber: '2347015424938', //Set your number here without + symbol.
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
